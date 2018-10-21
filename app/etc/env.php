18bb15fd2d378bf958e83dfafc8ebd963eb5167f
Victor Bodenko <bodenko.vv@gmail.com>
2018-10-21 15:41:10 +0300

    start home work 2

---
+++ app/etc/env.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'c4cc8dc0ea97201267748d2c9f3c5f91'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento_226',
+                'username' => 'magento_226',
+                'password' => 'magento_226',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'compiled_config' => 1
+    ],
+    'install' => [
+        'date' => 'Fri, 19 Oct 2018 09:36:37 +0000'
+    ]
+];
